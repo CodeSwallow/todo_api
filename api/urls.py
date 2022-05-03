9cfa6bac73b38bc00a35cdd0bee24ac9f0e2e32d
Isai Ramirez <isai.ramirez.stamaria15@gmail.com>
2022-05-03 08:50:37 -0500

    Added DefaultRouter for TodoListViewSet and TodoItemViewSet views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from api.views import main
+from api.views import TodoItemViewSet, TodoListViewSet
+
+router = DefaultRouter()
+router.register(r'lists', TodoListViewSet, basename='list')
+router.register(r'items', TodoItemViewSet, basename='item')
 
 urlpatterns = [
-    path('', main),
+    path('', include(router.urls)),
 ]
