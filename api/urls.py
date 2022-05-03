619efd2f05c26ee18686183ee4f9d89b18669d27
Isai Ramirez <isai.ramirez.stamaria15@gmail.com>
2022-05-03 17:51:53 -0500

    Added endpoints for registering and login in

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import TodoItemViewSet, TodoListViewSet
+from api.views import TodoItemViewSet, TodoListViewSet, user_view
 
 router = DefaultRouter()
 router.register(r'lists', TodoListViewSet, basename='list')
 router.register(r'items', TodoItemViewSet, basename='item')
 
 urlpatterns = [
+    path('user', user_view),
     path('', include(router.urls)),
 ]
