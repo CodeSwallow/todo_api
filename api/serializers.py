35f2a97dc6e00ee36563c4a32c8b70005d9bb4af
Isai Ramirez <isai.ramirez.stamaria15@gmail.com>
2022-05-03 19:17:39 -0500

    Override to update method in TodoItemSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -7,7 +8,10 @@ from api.models import TodoList, TodoItem
 class TodoListSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     status = serializers.ReadOnlyField()
-    duration = serializers.DurationField(read_only=True)
+    estimated_duration = serializers.DurationField(read_only=True)
+    started = serializers.ReadOnlyField()
+    finished = serializers.ReadOnlyField()
+    item_count = serializers.ReadOnlyField()
 
     class Meta:
         model = TodoList
@@ -15,6 +19,7 @@ class TodoListSerializer(serializers.ModelSerializer):
 
 
 class TodoItemSerializer(serializers.ModelSerializer):
+    finished = serializers.ReadOnlyField()
 
     class Meta:
         model = TodoItem
@@ -25,6 +30,21 @@ class TodoItemSerializer(serializers.ModelSerializer):
         representation['status'] = instance.get_status_display()
         representation['priority'] = instance.get_priority_display()
         return representation
+
+    def update(self, instance, validated_data):
+        item_status = validated_data.get('status', None)
+        if item_status is not None:
+            instance.status = validated_data.get('status', instance.status)
+            if instance.status == 'FI':
+                instance.finished = timezone.now()
+            else:
+                instance.finished = None
+        instance.name = validated_data.get('name', instance.name)
+        instance.priority = validated_data.get('priority', instance.priority)
+        instance.duration = validated_data.get('duration', instance.duration)
+        instance.started = validated_data.get('started', instance.started)
+        instance.save()
+        return instance
 
 
 class UserSerializer(serializers.ModelSerializer):
